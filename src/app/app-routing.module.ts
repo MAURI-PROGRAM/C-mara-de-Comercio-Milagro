aa15740652ae63a8484ab704f03a37e99fd1b06a
Michael Merchan <michael.merchan@iroute.com.ec>
2020-01-06 09:49:17 -0500

    agregando login al sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./Page/tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./login/signin/signin.module').then( m => m.SigninPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./page/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./login/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./login/signin/signin.module').then( m => m.SigninPageModule)
   }
 ];
 @NgModule({
