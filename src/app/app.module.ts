460e2debf16305b5e2582b2a20223a8a732a96bd
Michael <merchan.ber.mau@gmail.com>
2020-01-07 12:09:40 -0600

    Agregando modulos de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { EndpointProvider, DBkeys ,SessionStorageProvider} from '../providers/providers';
+import { EndpointProvider, DBkeys, SessionStorageProvider } from '../providers/providers';
 import { AuthService } from './../services/auth.service';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule],
   providers: [
     StatusBar,
-    EndpointProvider, 
-    SessionStorageProvider, 
+    EndpointProvider,
+    SessionStorageProvider,
     DBkeys,
     AuthService,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: FirestoreSettingsToken, useValue: {} }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
